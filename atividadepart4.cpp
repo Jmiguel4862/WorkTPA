31d1d44b12ebf7915f5df15d42834b32c89e705c
Jmiguel4862 <jmb04862@gmail.com>
2026-09-15 22:36:14 -0300

    primeiras modificações

--- atividadepart4.cpp
+++ atividadepart4.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <string.h>
+#include <iostream>
 
 struct Aluno{
     char matricula[9];
@@ -13,7 +14,7 @@ struct Aluno{
     char cidade[40];
 };
 
-#define TAMANHO_HASH_INICIAL 1000000
+#define TAMANHO_HASH_INICIAL 2000
 struct Alunos{
     Aluno **hash;
     bool *hashOcupada;
@@ -21,7 +22,32 @@ struct Alunos{
     int quantidade;
 };
 
+void inicializa();
+void lerArquivoCSV(const char* nomeArquivo);
+void adicionarAluno(Aluno *novo);
+void exibirAlunos();
+int calculoHash(char* nome);
+int calculoH2(char* nome);
+int calculoReHash(int resultadoCalculoAnterior, int resultadoH2);
+
+
 Alunos a;
+
+
+int main(){
+    inicializa();
+    printf("=== SISTEMA DE LEITURA DE ALUNOS CSV ===\n\n");
+    Aluno* alunoTemp;
+    alunoTemp = new Aluno;
+    a.hash[1548] = alunoTemp;
+    time_t inicio, fim;
+    inicio = clock();
+    lerArquivoCSV("alunos.csv");
+    fim = clock();
+    printf("Tempo de leitura: %d milissegundos\n", (int)fim - inicio);
+    system("pause");
+    return 0;
+}
 
 void inicializa(){
     a.tamanhoAtual = TAMANHO_HASH_INICIAL;
@@ -31,6 +57,41 @@ void inicializa(){
     for(int i=0; i<a.tamanhoAtual; i++){
         a.hashOcupada[i] = false;
     }
+}
+
+//Função para adiconar uma novo aluno
+
+void adicionarAluno(Aluno *novo){
+    int h1 = calculoHash(novo->nome), ind = h1;
+    if(a.quantidade == TAMANHO_HASH_INICIAL)
+    {
+          std::cout<<"\n\n Memoria cheia\n"<<std::endl;
+          delete novo;
+          return;
+        }
+    while (a.hashOcupada[ind])
+    {
+        if (novo->cpf == a.hash[ind]->cpf)
+        {
+            std::cout<<"\n\n [ERRO Aluno ja cadastrado]\n"<<std::endl;
+            delete novo;
+            return;
+        }
+        ind = calculoReHash(ind, calculoH2(novo->nome));
+        std::cout<<"\n Colição\n";
+        std::cout<<ind<<std::endl;
+    }
+
+    a.hash[ind] = novo;
+    a.hashOcupada[ind] = true;
+    a.quantidade++;
+    std::cout<<"\n\n outro aluno"<<std::endl;
+    std::cout<< a.quantidade <<std::endl;
+    std::cout<<ind<<std::endl;
+
+    system("pause");
+    
+
 }
 
 // Função para ler arquivo CSV
@@ -60,7 +121,7 @@ void lerArquivoCSV(const char* nomeArquivo) {
                    novo->matricula, novo->cpf, novo->nome, &novo->nota, &novo->idade, novo->curso, novo->cidade) == 7) {
             
             //pega o endereço que deve ser inserido no vetor de alunos
-            //adicionarAluno(0,novo);
+            adicionarAluno(novo);
             //printf("Aluno adicionado: %s - %s\n", novo->matricula, novo->nome);
             // Consumir a quebra de linha restante
             //fgetc(arquivo);
@@ -98,36 +159,19 @@ void exibirAlunos() {
 }
 
 int calculoHash(char* nome){
-    //faça uma função com base nos caracteres do nome do aluno
-    // K -> a conta sobre os caracteres
-
-    // retornar o calculo
-    // K mod TAMANHO_HASH_INICIAL
-
+    int total = 1;
+    for (int i = 0; i < strlen(nome); i++)
+        total = (total * nome[i])%TAMANHO_HASH_INICIAL;
+    return total;
 }
 
 int calculoH2(char* nome){
-    //faça uma função com base nos caracteres do nome do aluno
-    // K -> a conta sobre os caracteres
-
-    // retornar o calculo
-    // 1 + (K mod (TAMANHO_HASH_INICIAL - 1))
+    int total = 0;
+    for(int i = 0; i < strlen(nome); i++) 
+        total = (total + nome[i]) % TAMANHO_HASH_INICIAL;
+    return ((calculoHash(nome) + total) +1) % (TAMANHO_HASH_INICIAL - 1);
 }
 
 int calculoReHash(int resultadoCalculoAnterior, int resultadoH2){
     return (resultadoCalculoAnterior + resultadoH2) % TAMANHO_HASH_INICIAL;
-}
-int main(){
-    inicializa();
-    printf("=== SISTEMA DE LEITURA DE ALUNOS CSV ===\n\n");
-    Aluno* alunoTemp;
-    alunoTemp = new Aluno;
-    a.hash[1548] = alunoTemp;
-    time_t inicio, fim;
-    inicio = clock();
-    lerArquivoCSV("alunos.csv");
-    fim = clock();
-    printf("Tempo de leitura: %d milissegundos\n", (int)fim - inicio);
-    system("pause");
-    return 0;
 }
